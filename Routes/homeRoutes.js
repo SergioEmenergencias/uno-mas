eb13022dc552c69bb73b582c0f4d41ca2882056e
SergioEmenergencias <isaac8estrada@gmail.com>
2024-10-29 21:27:32 -0600

    Update homeRoutes.js

--- Routes/homeRoutes.js
+++ Routes/homeRoutes.js
@@ -2,7 +2,7 @@ const app= require('express')
 const { cargarActividades, cargahome, cargarForos, cargarFormularioCultivos, cargaformularioCosechas, cargaInformacionCultivo, preparacionTerreno, siembra, fertilizacion, enfermedades, produccion, venta, cargalogin }=require('../Controllers/CargaControllers')
 const { agregarPost, leerPublicaciones, prepararTerreno, crearSiembra, crearFertilizacion, crearEnfermedad, crearProduccion, crearVentas, leerForos } = require('../Controllers/HomeControllers')
 const Verificaruser = require('../Middleware/Verificaruser')
-const { insertarCosecha, insertarPlantacion, obtenerCosechas, obtenerPlantaciones, insertarCultivo, obtenerTodasTablas } = require('../Controllers/ActividadesControllers')
+const { insertarCosecha, insertarPlantacion, obtenerCosechas, obtenerPlantaciones, insertarCultivo, obtenerTodasTablas,descargarInforme } = require('../Controllers/ActividadesControllers')
 const router=app.Router()
 
 router.get('/', cargalogin)
